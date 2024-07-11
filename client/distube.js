cd9a2a7b5c97a56ab6b4b04ed8f44325c4316337
lanle <lanle101017@gmail.com>
2024-07-11 18:20:17 +0700

    update version

--- client/distube.js
+++ client/distube.js
@@ -2,13 +2,9 @@ const { DisTube } = require('distube');
 
 const { YtDlpPlugin } = require('@distube/yt-dlp');
 const { SoundCloudPlugin } = require('@distube/soundcloud');
-const { SpotifyPlugin } = require('@distube/spotify');
 const client = require('..');
 
-const { SPOTIFY_CLIENT_ID, SPOTIFY_SECRET_ID } = process.env;
-
 const distube = new DisTube(client, {
-    leaveOnStop: false,
     emitNewSongOnly: true,
     emitAddSongWhenCreatingQueue: false,
     emitAddListWhenCreatingQueue: false,
@@ -17,15 +13,6 @@ const distube = new DisTube(client, {
             update: false,
         }),
         new SoundCloudPlugin(),
-        new SpotifyPlugin({
-            emitEventsAfterFetching: false,
-            api: {
-                clientId: SPOTIFY_CLIENT_ID,
-                clientSecret: SPOTIFY_SECRET_ID,
-                topTracksCountry: 'VN',
-            },
-            parallel: true,
-        }),
     ],
 });
 
